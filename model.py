f382c1f1018030228ad87a21699eb5f3f2adc140
ThierryBleau <thierryb1996@gmail.com>
2019-03-12 17:07:07 -0400

    unpickling

--- model.py
+++ model.py
@@ -11,60 +11,24 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 from torch.autograd import Variable
 
-class Net(nn.Module):
+#todo -> pickle to tensor, transformations, bounding box selection, image classifictation, tuning
+# pretrained models? 
 
-    def __init__(self):
-        super(Net, self).__init__()
-        # 1 input image channel, 6 output channels, 5x5 square convolution
-        # kernel
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+data_folder = './data/'
+train_filename = data_folder + 'train_images.pkl'
+labels_filename = data_folder + 'train_labels.csv'
+test_filename = data_folder + 'test_images.pkl'
 
-    def forward(self, x):
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square you can only specify a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+def get_pickle_data(filename):
+    pickle_off = open(filename,'rb')
+    data = pkl.load(pickle_off)
+    return data
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+def get_labels_from_csv():
+    return pd.read_csv(labels_filename)
 
-net = Net()
-print(net)
-# loss function
-criterion = torch.nn.MSELoss(reduction='sum')
+image = get_pickle_data(train_filename)[0]
+print(image.shape)
+plt.imshow(image, interpolation='nearest')
+plt.show()
 
-# optimizer 
-optimizer = torch.optim.SGD(net.parameters(), lr=1e-4)
-
-net = Net()
-print(net)
-
-losses = []
-
-for epoch in range(50):
-    # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(x)
-
-    # Compute and print loss
-    loss = criterion(y_pred, y)
-    losses.append(loss.data.item())
-    print(f"Epoch : {epoch}    Loss : {loss.data.item()}")
-
-    # Reset gradients to zero, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
