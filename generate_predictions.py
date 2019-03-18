eefe967964e16d9f5f2ee986df7d4051fcf77966
ThierryBleau <thierryb1996@gmail.com>
2019-03-18 00:16:23 -0400

    predicitions

---
+++ generate_predictions.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import pickle as pkl
+import time
+import copy
+import math
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F 
+from torchvision import datasets, transforms, models
+from torch.autograd import Variable
+from sklearn.model_selection import train_test_split
+from resnet import ResNet, BasicBlock
+
+data_folder = './data/'
+kaggle_filename = data_folder + 'test_images.pkl'
+kaggle_images = pd.read_pickle(kaggle_filename)
+
+f = lambda x: 0 if x < 255 else 255
+vf = np.vectorize(f)
+for i in range(len(kaggle_images)):
+    for j in range(len(kaggle_images[i])):
+        vf(kaggle_images[i][j])
+
+torch_kaggle = torch.from_numpy(kaggle_images).type(torch.LongTensor)
+torch_kaggle = torch_kaggle.view(-1, 1,64,64).float()
+
+kaggle = torch.utils.data.TensorDataset(torch_kaggle)
+kaggle_loader = torch.utils.data.DataLoader(kaggle, batch_size = 1, shuffle = False)
+
+model = ResNet(BasicBlock, [2, 2, 2, 2])
+print(model)
+model.load_state_dict(torch.load('./resnet_model4.ckpt'))
+print(model)
+
+predictions = []
+model.eval()
+for x in kaggle_loader:
+    outputs = model(x[0])
+    predicted = torch.max(outputs.data, 1)
+    #print(predicted[1].item())
+    predictions.append(predicted[1].item())
+    #print(predictions)
+
+x = [x for x in range(0,10000)]
+df = pd.DataFrame(data={"ID": x, "Category": predictions})
+df.to_csv("./submission_filtered2.csv", sep=',',index=False)
